a105415ff65196c01846a99385064729d76187cc
Sri <srilalitha.ganti@gmail.com>
2022-01-18 15:55:17 +1100

    re-styled homepage

--- reactportfolio/src/components/About.js
+++ reactportfolio/src/components/About.js
@@ -8,27 +8,17 @@ function About() {
       <h2 class="about-me">About Me</h2>
       <img className="profile-img" id="profile" src={meimg} alt="me" />
       <div class="description">
-        <b>Hi! I am Srilalitha and I am a full-stack Web Developer</b>
-        <b>
-          I love problem solving, using my creativity and critical thinking to
-          find solutions.
-        </b>
-        <b>
-          I also enjoy working in collaboration with others to find even better
-          solutions.
-        </b>
-        <b>
-          Having a curious mind, I don’t mind stepping out of my comfort zone
-          because I like the stimulation of learning new things - that space of
-          continuous learning that leads to improvement.
-        </b>
-        <b>
-          I also enjoy the process of transforming ideas into reality and making
-          things happen.
-        </b>
-        In addition, I want to create a positive difference and becoming a
-        developer is where I feel I can contribute my skills and passion
+        Hi! I am Srilalitha and I am a full-stack Web Developer I love problem
+        solving, using my creativity and critical thinking to find solutions. I
+        also enjoy working in collaboration with others to find even better
+        solutions. Having a curious mind, I don’t mind stepping out of my
+        comfort zone because I like the stimulation of learning new things -
+        that space of continuous learning that leads to improvement. I also
+        enjoy the process of transforming ideas into reality and making things
+        happen. In addition, I want to create a positive difference and becoming
+        a developer is where I feel I can contribute my skills and passion
       </div>
+
     </section>
   );
 }
--- reactportfolio/src/components/Header.js
+++ reactportfolio/src/components/Header.js
@@ -2,37 +2,36 @@ import React from "react";
 import "../App.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faGithub, faLinkedin } from "@fortawesome/free-brands-svg-icons";
-import { faEnvelope } from "@fortawesome/free-solid-svg-icons";
 
 function Header() {
   return (
     <div id="home">
-      <header>
-        <h1>
-          Hello! I'm <span>Srilalitha </span>a full-stack developer.
-        </h1>
-
-        <a href="https://github.com/SrilalithaN">
-          <FontAwesomeIcon
-            icon={faGithub}
-            className="fa fa-github"
-          ></FontAwesomeIcon>
-        </a>
-
-        <a href="www.linkedin.com/in/srilalitha-nookala">
-          <FontAwesomeIcon
-            icon={faLinkedin}
-            className="fa fa-linkedin"
-          ></FontAwesomeIcon>
-        </a>
-
-        <a href="mailto:srilalitha.ganti@gmail.com">
-          <FontAwesomeIcon
-            icon={faEnvelope}
-            className="fa fa-envelope"
-          ></FontAwesomeIcon>
-        </a>
+      <header className="header">
+        <h1 className="text-center">Srilalitha</h1>
       </header>
+      <div className="shortBio">
+        <p className="shortBio">
+          Full Stack Developer. Passionate Programmer. Always Learning!
+        </p>
+
+        <span>
+          <a href="https://github.com/SrilalithaN">
+            <FontAwesomeIcon
+              icon={faGithub}
+              className="fa fa-github"
+            ></FontAwesomeIcon>
+          </a>
+        </span>
+
+        <span>
+          <a href="www.linkedin.com/in/srilalitha-nookala">
+            <FontAwesomeIcon
+              icon={faLinkedin}
+              className="fa fa-linkedin"
+            ></FontAwesomeIcon>
+          </a>
+        </span>
+      </div>
     </div>
   );
 }
